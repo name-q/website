19c4de0f8ef9c09142e0ac0345df0c451f4ce96d
name-q <qyking@outlook.com>
2018-05-13 22:26:39 +0800

    time err
    solve:time Fixed value

--- libs/write_log.js
+++ libs/write_log.js
@@ -1,31 +1,23 @@
-// index.js
+
 
 let fs = require('fs');
 
-let options = {
-  flags: 'a',     // append模式
-  encoding: 'utf8',  // utf8编码
-};
 
-let success = fs.createWriteStream('./log/success.log', options);
-let error = fs.createWriteStream('./log/error.log', options);
 
-// 创建logger
-let log= new console.Console(success, error);
+let success = fs.createWriteStream('./log/success.log', {flags: 'a',encoding: 'utf8'});
+let error = fs.createWriteStream('./log/error.log', {flags: 'a',encoding: 'utf8'});
 
-// 添加format方法
+
+let qy= new console.Console(success, error);
+
+
 Date.prototype.format = function (format) {
 
-  if (!format) {
-    format = 'yyyy-MM-dd HH:mm:ss';
-  }
-
-  // 用0补齐指定位数
   let padNum = function (value, digits) {
     return Array(digits - value.toString().length + 1).join('0') + value;
   };
 
-  // 指定格式字符
+
   let cfg = {
     yyyy: this.getFullYear(),             // 年
     MM: padNum(this.getMonth() + 1, 2),        // 月
@@ -33,7 +25,6 @@ Date.prototype.format = function (format) {
     HH: padNum(this.getHours(), 2),          // 时
     mm: padNum(this.getMinutes(), 2),         // 分
     ss: padNum(this.getSeconds(), 2),         // 秒
-    fff: padNum(this.getMilliseconds(), 3),      // 毫秒
   };
 
   return format.replace(/([a-z]|[A-Z])(\1)*/ig, function (m) {
@@ -42,13 +33,16 @@ Date.prototype.format = function (format) {
 }
 
 
-  let time = new Date().format('yyyy-MM-dd HH:mm:ss.fff');
-///___
+  function getNewTime(){
+    return new Date().format('yyyy-MM-dd HH:mm:ss');
+  }
+
+
 
   module.exports={
     success:(value)=>{
-    log.log(`[${time}] - success message |${value}`);},
+      qy.log(`[${getNewTime()}] - success message |${value}`);},
 
     error:(value)=>{
-    log.error(`[${time}] - error message |${value}`);}
+      qy.error(`[${getNewTime()}] - error message |${value}`);}
   }
