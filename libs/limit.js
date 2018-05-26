a33405db077fc2639474d4336b150e748aff925a
name-q <qyking@outlook.com>
2018-05-26 21:19:45 +0800

    +ev limit

--- libs/limit.js
+++ libs/limit.js
@@ -1,8 +1,12 @@
 const limit={
-  ////ad_banners page [1]   english_letter=>0.5 chinese=>1
+  ////ad_banners ad_nav_url[0]   english_letter=>0.5 chinese=>1
   title:6,  //db <32
   description:20, //db <300
 
+  ////ad_evaluate ad_nav_url[3]
+  ev_title:6,
+  ev_description:20,
+  ev_suffix_ok:["jpg","png"],
   //
 
 }
